32a58215586b440d7701d6aef5addb2aeabb2e5c
Anezeres <alvarez.julian@correounivalle.edu.co>
2023-09-26 15:50:11 -0500

    Ya se puede mostrar el mapa

--- Ventana.py
+++ Ventana.py
@@ -44,6 +44,22 @@ def dibujar_imagenes_en_fila(lienzo, imagen, mapa):
             if(mapa[fila][columna] == 1):
                 imagen2 = lienzo.create_image(posX, posY, anchor=tk.NW, image=imagen[1])
                 lienzo.lift(imagen2)
+            elif(mapa[fila][columna] == 2):
+                imagen2 = lienzo.create_image(posX, posY, anchor=tk.NW, image=imagen[2])
+                lienzo.lift(imagen2)
+
+            elif(mapa[fila][columna] == 6):
+                imagen2 = lienzo.create_image(posX, posY, anchor=tk.NW, image=imagen[3])
+                lienzo.lift(imagen2)
+            elif(mapa[fila][columna] == 5):
+                imagen2 = lienzo.create_image(posX, posY, anchor=tk.NW, image=imagen[4])
+                lienzo.lift(imagen2)
+            elif(mapa[fila][columna] == 3):
+                imagen2 = lienzo.create_image(posX, posY, anchor=tk.NW, image=imagen[5])
+                lienzo.lift(imagen2)
+            elif(mapa[fila][columna] == 4):
+                imagen2 = lienzo.create_image(posX, posY, anchor=tk.NW, image=imagen[6])
+                lienzo.lift(imagen2)
 
             """ else:   
                 lienzo.create_image(posX, posY, anchor=tk.NW, image=imagen[0]) """
@@ -54,12 +70,35 @@ def dibujar_imagenes_en_fila(lienzo, imagen, mapa):
 
 def crearSprites():
 
+    orcoPng = Image.open("Sprites/Orco.png")
+    soldierPng = Image.open("Sprites/Soldier.png")
+    potionPng = Image.open("Sprites/Potion.png")
+    swordPng = Image.open("Sprites/Sword.png")
+    superSwordPng = Image.open("Sprites/SuperSword.png")
+    potionGrande = potionPng.resize((64, 64), Image.ANTIALIAS)
+    soldierGrande = soldierPng.resize((39,62), Image.ANTIALIAS)
+    swordGrande = swordPng.resize((64,64), Image.ANTIALIAS)
+    superSwordGrande = superSwordPng.resize((64,64), Image.ANTIALIAS)
+
     sprites = []
     imagenPasto = crearSprite(19,8)
     imagenPiedra = crearSprite(17,4)
+    imagenFuego = crearSprite(0,12)
+    
+    orco = ImageTk.PhotoImage(orcoPng)
+    soldier = ImageTk.PhotoImage(soldierGrande)
+    potion = ImageTk.PhotoImage(potionGrande)
+    sword = ImageTk.PhotoImage(swordGrande)
+    superSword = ImageTk.PhotoImage(superSwordGrande)
 
     sprites.append(imagenPasto)
     sprites.append(imagenPiedra)
+    sprites.append(orco)
+    sprites.append(potion)
+    sprites.append(soldier)
+    sprites.append(sword)
+    sprites.append(superSword)
+    #sprites.append(imagenFuego)
 
     return sprites 
 
